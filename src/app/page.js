564b05e1f5d482517695c5ee60e5b476ca1ab16f
Jose Soto <jsoto7087@gmail.com>
2024-05-23 22:22:55 -0400

    adds products

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Hero from "./Hero";
-
+import Products from "./Products";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Products />
     </div>
   );
 }
